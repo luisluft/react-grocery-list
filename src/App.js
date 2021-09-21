4117b9b5da23c69d8680b7f6bb54a8f95070a25b
luisluft <luberluft@gmail.com>
2021-09-21 08:49:45 -0400

    add raw UI elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { useState, useEffect } from "react";
 import List from "./List";
 import Alert from "./Alert";
+import { FaLeaf } from "react-icons/fa";
 
 function App() {
-  return <h2>grocery bud setup</h2>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, message: "", type: "" });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery list</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List></List>
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
